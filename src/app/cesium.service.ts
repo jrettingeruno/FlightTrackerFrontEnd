bdcc24fdc01ee564ea2d3240a15fcc03392da923
Derek <dheimes@unomaha.edu>
2023-04-17 19:08:04 -0500

    Fixed some errors after merge

--- src/app/cesium.service.ts
+++ src/app/cesium.service.ts
@@ -1,10 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RectangleNoFly } from './objects/rectangle-no-fly/rectangle-no-fly';
 import { PolygonNoFly } from './objects/polygon-no-fly/polygon-no-fly';
 import { EllipsoidNoFly } from './objects/ellipsoid-no-fly/ellipsoid-no-fly';
+import { GetNoFlyZonesResponse } from './objects/get-no-fly-zones-response/get-no-fly-zones-response';
 
 declare let Cesium: any;
+let global_viewer: any;
+let global_coord_array: number[];
 
 Cesium.Ion.defaultAccessToken =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxMTE4ZTAwNC1iYjY0LTQ2MmUtOTAzNi0wZDNhY2EyY2MzNTAiLCJpZCI6MTI4NTgxLCJpYXQiOjE2Nzg3NjAzNTB9.1UEATVrKMPSwbEh6ObHPAMs1L99KpJg_pKqMjVDqYDk";
@@ -248,6 +251,55 @@ plotPoints(div:string){
     //     color: Cesium.Color.YELLOW,
     //     pixelSize: 16,
     //   },
+    // });
+  }
+
+  flyToAndPlotPoint(div: string, longitude: number, latitude: number, altitude: number) {
+    if (global_coord_array == undefined) {
+      global_coord_array = []
+    }
+
+    global_coord_array.push(longitude)
+    global_coord_array.push(latitude)
+    global_coord_array.push(altitude)
+
+
+    if (global_viewer == null || global_viewer == undefined) {
+      global_viewer = new Cesium.Viewer(div)
+    }
+    global_viewer.infoBox.frame.removeAttribute("sandbox");
+    global_viewer.infoBox.frame.src = "about:blank";
+    global_viewer.scene.requestRenderMode = false;
+
+    global_viewer.entities.add({
+      position: Cesium.Cartesian3.fromDegrees(longitude, latitude, altitude),
+      point: {
+        color: Cesium.Color.RED,
+        pixelSize: 16,
+      },
+    });
+
+
+    console.log(global_coord_array)
+
+    global_viewer.entities.add({
+      name: "FlightPath",
+      polyline: {
+        positions: Cesium.Cartesian3.fromDegreesArrayHeights(global_coord_array),
+        width: 10,
+        material: Cesium.Color.RED,
+        clampToGround: false,
+      },
+    });
+
+
+
+    // global_viewer.camera.flyTo({
+    //   destination: Cesium.Cartesian3.fromDegrees(longitude, latitude, altitude),
+    //   orientation: {
+    //     heading: Cesium.Math.toRadians(0.0),
+    //     pitch: Cesium.Math.toRadians(0.0),
+    //   }
     // });
   }
 }
